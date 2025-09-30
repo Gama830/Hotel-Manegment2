2cf63b385988ba2ec8016b0dfb05a8faed9e2d8d
Harshit Malviya <harshitmalviya127@gmail.com>
2025-09-30 19:21:18 +0530

    fix booking_form submit

--- booking_master/models.py
+++ booking_master/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from rooms.models import RoomType
 from reservation_source_master.models import ReservationSource
+from timeslotmaster.models import TimeslotMaster
 
 class Booking(models.Model):
     ID_PROOF_CHOICES = [
@@ -27,43 +28,9 @@ class Booking(models.Model):
     booking_date = models.DateField(auto_now_add=True)
     booking_time = models.TimeField(auto_now_add=True)
     room_type = models.ForeignKey(RoomType, on_delete=models.PROTECT)
+    time_slot = models.ForeignKey(TimeslotMaster, on_delete=models.PROTECT)
     payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES)
-    reservation_source = models.ForeignKey(ReservationSource, on_delete=models.SET_NULL, null=True, blank=True, help_text="Source of this booking")
-    applied_discounts = models.ManyToManyField('discount_master.DiscountMaster', blank=True, help_text="Discounts applied to this booking")
-    base_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0, help_text="Base booking amount before discounts")
-    discount_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0, help_text="Total discount amount")
-    final_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0, help_text="Final amount after discounts")
     reservation_source = models.ForeignKey(ReservationSource, on_delete=models.PROTECT)
-
-    def calculate_discounts(self):
-        """Calculate total discount amount based on applied discounts"""
-        total_discount = 0
-        for discount in self.applied_discounts.all():
-            if discount.discount_value.endswith('%'):
-                percent = float(discount.discount_value.rstrip('%'))
-                total_discount += self.base_amount * (percent / 100)
-            else:
-                total_discount += float(discount.discount_value)
-        return total_discount
-    
-    def update_final_amount(self):
-        """Update final amount after calculating discounts"""
-        self.discount_amount = self.calculate_discounts()
-        self.final_amount = self.base_amount - self.discount_amount
-        if self.final_amount < 0:
-            self.final_amount = 0
-    
-    def get_available_discounts(self):
-        """Get available discounts from reservation source"""
-        if self.reservation_source:
-            return self.reservation_source.get_applicable_discounts()
-        return []
-    
-    def save(self, *args, **kwargs):
-        # Calculate final amount before saving
-        if self.base_amount > 0:
-            self.update_final_amount()
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Booking {self.booking_id} - {self.customer_first_name} {self.customer_last_name}"
